bebceb5c877229667ca1f2594edf654fd047111d
Martin W <nodexchange@outlook.com>
2016-03-30 08:34:54 +0100

    initial files

---
+++ app/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div className='alert alert-info'>Hello from Home Component</div>);
+  }
+}
